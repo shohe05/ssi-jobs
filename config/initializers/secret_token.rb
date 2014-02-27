7909e19e9b9384cc5945d631a280820ebb56cae2
Syouhei Tanaka <shou.tanak@gmail.com>
2014-02-27 18:44:23 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SsiJobs::Application.config.secret_key_base = 'cafd46882a9fcdbe4fa0c880210dc33b0b571646cd0cd86ef2681acb052ee8788b33f1c889c6732d305212375a28d4f077fb3d1eaceff4bdc45861ac071319bf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SsiJobs::Application.config.session_store :cookie_store, key: '_ssi_jobs_session'
